a52f441e9b93540434f738815a720959d3b86046
Neel <neeljadhav0210@gmail.com>
2021-12-28 21:49:36 +0530

    Update Debloater.bat

--- Debloater.bat
+++ Debloater.bat
@@ -15,42 +15,42 @@ timeout /t 5 /nobreak > nul
 echo  Removing below mentioned services/apps
 @echo off
 ######################################################
-#  To uninstall user apps : pm uninstall -k --user 0 #
+#  To uninstall user apps : pm uninstall -k --user 0 --user 0 #
 #  To disable them : pm disable-user --user 0        #
 ######################################################
 echo (--------------------------------------)
 echo                Facebook apps
 echo (--------------------------------------)
-adb shell pm uninstall -k com.facebook.services
-adb shell pm uninstall -k com.facebook.katana
-adb shell pm uninstall -k com.facebook.system
-adb shell pm uninstall -k com.facebook.appmanager
-adb shell pm uninstall -k com.vanced.android.youtube
-adb shell pm uninstall -k com.facebook.system
-adb shell pm uninstall -k com.facebook.appmanager
-adb shell pm uninstall -k com.facemoji.lite.xiaomi
-adb shell pm uninstall –k ––user 0 com.facebook.appmanager
-adb shell pm uninstall –k ––user 0 com.facebook.services
-adb shell pm uninstall –k ––user 0 com.facebook.system
-adb shell pm uninstall -k com.sec.android.app.DataCreate
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.vanced.android.youtube
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facemoji.lite.xiaomi
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.sec.android.app.DataCreate
 timeout /t 2 /nobreak > nul
 echo (--------------------------------------)
 echo                 Google apps
 echo (--------------------------------------)
-adb shell pm uninstall -k com.android.chrome
-adb shell pm uninstall -k com.google.android.youtube
-adb shell pm uninstall -k com.google.android.tts
-adb shell pm uninstall -k com.google.android.googlequicksearchbox
-adb shell pm uninstall -k com.google.android.apps.tachyon
-adb shell pm uninstall -k com.google.android.apps.docs
-adb shell pm uninstall -k com.google.android.gm
-adb shell pm uninstall -k com.google.android.videos
-adb shell pm uninstall -k com.google.android.music
-adb shell pm uninstall -k com.google.android.tts
-adb shell pm uninstall -k com.google.android.apps.maps
-adb shell pm uninstall -k com.google.android.apps.photos
-adb shell pm uninstall -k com.google.android.youtube
-adb shell pm uninstall -k com.google.ar.lens
+adb shell pm uninstall -k --user 0 com.android.chrome
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+adb shell pm uninstall -k --user 0 com.google.android.tts
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs
+adb shell pm uninstall -k --user 0 com.google.android.gm
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.google.android.music
+adb shell pm uninstall -k --user 0 com.google.android.tts
+adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+adb shell pm uninstall -k --user 0 com.google.ar.lens
 adb shell pm disable-user --user 0 com.google.ar.lens
 adb shell pm disable-user --user 0 com.google.android.youtube
 adb shell pm disable-user --user 0 com.google.android.gm
@@ -60,130 +60,130 @@ timeout /t 2 /nobreak > nul
 echo (--------------------------------------)
 echo                Microsoft apps
 echo (--------------------------------------)
-adb shell pm uninstall -k com.microsoft.office.powerpoint
-adb shell pm uninstall -k com.microsoft.office.excel
-adb shell pm uninstall -k com.microsoft.office.word
-adb shell pm uninstall -k com.microsoft.skydrive
+adb shell pm uninstall -k --user 0 com.microsoft.office.powerpoint
+adb shell pm uninstall -k --user 0 com.microsoft.office.excel
+adb shell pm uninstall -k --user 0 com.microsoft.office.word
+adb shell pm uninstall -k --user 0 com.microsoft.skydrive
 timeout /t 2 /nobreak > nul
 echo (--------------------------------------)
 echo                Linkedin Apps
 echo (--------------------------------------)
-adb shell pm uninstall -k com.linkedin.android
+adb shell pm uninstall -k --user 0 com.linkedin.android
 timeout /t 2 /nobreak > nul
 echo (--------------------------------------)
 echo                Samsung Apps
 echo (--------------------------------------)
-adb shell pm uninstall -k com.sec.android.app.sbrowser
-adb shell pm uninstall -k com.sec.android.email.provider
-adb shell pm uninstall -k com.sec.android.app.voicenote
-adb shell pm uninstall -k com.sec.android.beaconmanage
-adb shell pm uninstall -k com.sec.android.dynamiclock
-adb shell pm uninstall -k com.sec.android.dynsystem
-adb shell pm uninstall -k com.sec.android.app.samsungapps
-adb shell pm uninstall -k com.sec.android.game.gameho
-adb shell pm uninstall -k com.sec.android.game.gametool
-adb shell pm uninstall -k com.sec.android.service.health
-adb shell pm uninstall -k com.sec.android.icecone
-adb shell pm uninstall -k com.sec.android.keyguardmgsup
-adb shell pm uninstall -k com.sec.android.sprotect
-adb shell pm uninstall -k com.sec.android.securefolder
-adb shell pm uninstall -k com.sec.android.app.camera.sticker.facear.preload
-adb shell pm uninstall -k com.sec.android.themecenter
-adb shell pm uninstall -k com.sec.android.themestore
-adb shell pm uninstall -k com.sec.android.app.sbrowser.lite
+adb shell pm uninstall -k --user 0 com.sec.android.app.sbrowser
+adb shell pm uninstall -k --user 0 com.sec.android.email.provider
+adb shell pm uninstall -k --user 0 com.sec.android.app.voicenote
+adb shell pm uninstall -k --user 0 com.sec.android.beaconmanage
+adb shell pm uninstall -k --user 0 com.sec.android.dynamiclock
+adb shell pm uninstall -k --user 0 com.sec.android.dynsystem
+adb shell pm uninstall -k --user 0 com.sec.android.app.samsungapps
+adb shell pm uninstall -k --user 0 com.sec.android.game.gameho
+adb shell pm uninstall -k --user 0 com.sec.android.game.gametool
+adb shell pm uninstall -k --user 0 com.sec.android.service.health
+adb shell pm uninstall -k --user 0 com.sec.android.icecone
+adb shell pm uninstall -k --user 0 com.sec.android.keyguardmgsup
+adb shell pm uninstall -k --user 0 com.sec.android.sprotect
+adb shell pm uninstall -k --user 0 com.sec.android.securefolder
+adb shell pm uninstall -k --user 0 com.sec.android.app.camera.sticker.facear.preload
+adb shell pm uninstall -k --user 0 com.sec.android.themecenter
+adb shell pm uninstall -k --user 0 com.sec.android.themestore
+adb shell pm uninstall -k --user 0 com.sec.android.app.sbrowser.lite
 timeout /t 2 /nobreak > nul
 echo (--------------------------------------)
 echo                 Bixby apps
 echo (--------------------------------------)
-adb shell pm uninstall -k com.samsung.android.bixby.wakeup
-adb shell pm uninstall -k com.samsung.android.bixby.service
-adb shell pm uninstall -k com.samsung.android.visionintelligence
-adb shell pm uninstall -k com.samsung.android.bixby.agent
-adb shell pm uninstall -k com.samsung.android.bixby.agent.dummy
-adb shell pm uninstall -k com.samsung.android.bixbyvision.framework
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.wakeup
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.service
+adb shell pm uninstall -k --user 0 com.samsung.android.visionintelligence
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent
+adb shell pm uninstall -k --user 0 com.samsung.android.bixby.agent.dummy
+adb shell pm uninstall -k --user 0 com.samsung.android.bixbyvision.framework
 timeout /t 2 /nobreak > nul
 echo (----------------------------------------------------------)
 echo   Realme apps, ignore if u dont have redmi device and wait 
 echo (----------------------------------------------------------)
-adb shell pm uninstall -k com.coloros.compass2
-adb shell pm uninstall -k com.heytap.market
-adb shell pm uninstall -k com.redteamobile.roaming
-adb shell pm uninstall -k com.heytap.market
-adb shell pm disable–user ––user 0 com.heytap.market
-adb shell pm uninstall -k --user 0 com.heytap.browser
+adb shell pm uninstall -k --user 0 com.coloros.compass2
 adb shell pm uninstall -k --user 0 com.heytap.market
 adb shell pm uninstall -k --user 0 com.redteamobile.roaming
-adb shell pm uninstall -k --user 0 com.heytap.usercenter
-adb shell pm uninstall -k --user 0 com.heytap.cloud
-adb shell pm uninstall -k --user 0 com.oppo.quicksearchbox
-adb shell pm uninstall -k --user 0 com.coloros.backuprestore
-adb shell pm uninstall -k --user 0 com.coloros.weather2
-adb shell pm uninstall -k --user 0 com.coloros.weather.service
+adb shell pm uninstall -k --user 0 com.heytap.market
+adb shell pm disable --user 0 com.heytap.market
+adb shell pm uninstall -k --user 0 --user 0 com.heytap.browser
+adb shell pm uninstall -k --user 0 --user 0 com.heytap.market
+adb shell pm uninstall -k --user 0 --user 0 com.redteamobile.roaming
+adb shell pm uninstall -k --user 0 --user 0 com.heytap.usercenter
+adb shell pm uninstall -k --user 0 --user 0 com.heytap.cloud
+adb shell pm uninstall -k --user 0 --user 0 com.oppo.quicksearchbox
+adb shell pm uninstall -k --user 0 --user 0 com.coloros.backuprestore
+adb shell pm uninstall -k --user 0 --user 0 com.coloros.weather2
+adb shell pm uninstall -k --user 0 --user 0 com.coloros.weather.service
 pm disable-user --user 0 com.nearme.gamecenter
 timeout /t 2 /nobreak > nul
 echo (--------------------------------------------------------)
 echo  Redmi-apps, ignore if u dont have redmi device and wait
 echo (--------------------------------------------------------)
-adb shell pm uninstall -k com.miui.cleanmaster
-adb shell pm uninstall -k com.xiaomi.mipicks
-adb shell pm uninstall -k com.xiaomi.payment
-adb shell pm uninstall -k com.mipay.wallet.in
-adb shell pm uninstall -k com.xiaomi.midrop
-adb shell pm uninstall -k com.miui.miservice
-adb shell pm uninstall -k com.miui.daemon
-adb shell pm uninstall -k com.miui.msa.global
-adb shell pm uninstall -k com.mi.webkit.core
-adb shell pm uninstall -k com.xiaomi.joyose
-adb shell pm uninstall -k com.facebook.services
-adb shell pm uninstall -k com.facebook.appmanager
-adb shell pm uninstall -k com.facebook.system
-adb shell pm uninstall -k com.miui.analytics
-adb shell pm uninstall -k com.miui.android.fashiongallery
-adb shell pm uninstall -k com.xiaomi.midrop
-adb shell pm uninstall -k com.android.browser
-adb shell pm uninstall -k com.android.calendar
-adb shell pm uninstall -k com.miui.bugreport
-adb shell pm uninstall -k com.xiaomi.glgm
-adb shell pm uninstall -k com.miui.yellowpage
-adb shell pm uninstall -k com.miui.weather2
-adb shell pm uninstall -k com.miui.miservice
-adb shell pm uninstall -k com.miui.player
-adb shell pm uninstall -k com.mipay.wallet.id
-adb shell pm uninstall -k com.mipay.wallet.in
-adb shell pm uninstall -k com.xiaomi.payment
-adb shell pm uninstall -k com.miui.hybrid
-adb shell pm uninstall -k com.miui.hybrid.accessory
-adb shell pm uninstall -k com.miui.videoplayer
-adb shell pm uninstall -k com.xiaomi.mipicks
-adb shell pm uninstall -k com.miui.translation.kingsoft
-adb shell pm uninstall -k com.miui.translation.youdao
-adb shell pm uninstall -k com.miui.translation.xmcloud
-adb shell pm uninstall -k com.miui.translationservice
-adb shell pm uninstall -k cn.wps.xiaomi.abroad.lite
-adb shell pm uninstall -k com.wps.xiaomi.abroad.lite
-adb shell pm uninstall -k com.xiaomi.mirecycle
-adb shell pm uninstall -k com.xunmeng.pinduoduo
-adb shell pm uninstall -k com.xiaomi.midrop
-adb shell pm uninstall -k com.tencent.mtt
-adb shell pm uninstall -k com.eg.android.alipayGphone
-adb shell pm uninstall -k com.dragon.read
-adb shell pm uninstall -k com.taobao.taobao
-adb shell pm uninstall -k com.tencent.qqlive
-adb shell pm uninstall -k com.qiyi.video
-adb shell pm uninstall -k com.ss.android.article.news
-adb shell pm uninstall -k com.wps.moffice_eng
-adb shell pm uninstall -k com.sina.weibo
-adb shell pm uninstall -k com.xiaomi.jr
-adb shell pm uninstall -k com.ss.android.ugc.aweme
-adb shell pm uninstall -k com.baidu.BaiduMap
-adb shell pm uninstall -k com.xiaomi.youpin
-adb shell pm uninstall -k com.duokan.reader
-adb shell pm uninstall -k com.xiaomi.vipaccount
-adb shell pm uninstall -k com.mipay.wallet
-adb shell pm uninstall -k com.miui.miservice
-adb shell pm uninstall -k com.android.email
-adb shell pm uninstall -k com.miui.global.packageinstaller
-adb shell pm uninstall -k android.autoinstalls.config.Xiaomi.ginkgo
+adb shell pm uninstall -k --user 0 com.miui.cleanmaster
+adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+adb shell pm uninstall -k --user 0 com.xiaomi.payment
+adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall -k --user 0 com.miui.daemon
+adb shell pm uninstall -k --user 0 com.miui.msa.global
+adb shell pm uninstall -k --user 0 com.mi.webkit.core
+adb shell pm uninstall -k --user 0 com.xiaomi.joyose
+adb shell pm uninstall -k --user 0 com.facebook.services
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.miui.analytics
+adb shell pm uninstall -k --user 0 com.miui.android.fashiongallery
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.android.browser
+adb shell pm uninstall -k --user 0 com.android.calendar
+adb shell pm uninstall -k --user 0 com.miui.bugreport
+adb shell pm uninstall -k --user 0 com.xiaomi.glgm
+adb shell pm uninstall -k --user 0 com.miui.yellowpage
+adb shell pm uninstall -k --user 0 com.miui.weather2
+adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall -k --user 0 com.miui.player
+adb shell pm uninstall -k --user 0 com.mipay.wallet.id
+adb shell pm uninstall -k --user 0 com.mipay.wallet.in
+adb shell pm uninstall -k --user 0 com.xiaomi.payment
+adb shell pm uninstall -k --user 0 com.miui.hybrid
+adb shell pm uninstall -k --user 0 com.miui.hybrid.accessory
+adb shell pm uninstall -k --user 0 com.miui.videoplayer
+adb shell pm uninstall -k --user 0 com.xiaomi.mipicks
+adb shell pm uninstall -k --user 0 com.miui.translation.kingsoft
+adb shell pm uninstall -k --user 0 com.miui.translation.youdao
+adb shell pm uninstall -k --user 0 com.miui.translation.xmcloud
+adb shell pm uninstall -k --user 0 com.miui.translationservice
+adb shell pm uninstall -k --user 0 cn.wps.xiaomi.abroad.lite
+adb shell pm uninstall -k --user 0 com.wps.xiaomi.abroad.lite
+adb shell pm uninstall -k --user 0 com.xiaomi.mirecycle
+adb shell pm uninstall -k --user 0 com.xunmeng.pinduoduo
+adb shell pm uninstall -k --user 0 com.xiaomi.midrop
+adb shell pm uninstall -k --user 0 com.tencent.mtt
+adb shell pm uninstall -k --user 0 com.eg.android.alipayGphone
+adb shell pm uninstall -k --user 0 com.dragon.read
+adb shell pm uninstall -k --user 0 com.taobao.taobao
+adb shell pm uninstall -k --user 0 com.tencent.qqlive
+adb shell pm uninstall -k --user 0 com.qiyi.video
+adb shell pm uninstall -k --user 0 com.ss.android.article.news
+adb shell pm uninstall -k --user 0 com.wps.moffice_eng
+adb shell pm uninstall -k --user 0 com.sina.weibo
+adb shell pm uninstall -k --user 0 com.xiaomi.jr
+adb shell pm uninstall -k --user 0 com.ss.android.ugc.aweme
+adb shell pm uninstall -k --user 0 com.baidu.BaiduMap
+adb shell pm uninstall -k --user 0 com.xiaomi.youpin
+adb shell pm uninstall -k --user 0 com.duokan.reader
+adb shell pm uninstall -k --user 0 com.xiaomi.vipaccount
+adb shell pm uninstall -k --user 0 com.mipay.wallet
+adb shell pm uninstall -k --user 0 com.miui.miservice
+adb shell pm uninstall -k --user 0 com.android.email
+adb shell pm uninstall -k --user 0 com.miui.global.packageinstaller
+adb shell pm uninstall -k --user 0 android.autoinstalls.config.Xiaomi.ginkgo
 adb shell pm disable-user --user 0 com.xiaomi.mipicks
 adb shell pm disable-user --user 0 com.mipay.wallet.in
 adb shell pm disable-user --user 0 com.xiaomi.midrop

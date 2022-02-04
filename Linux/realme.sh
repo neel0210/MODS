2af7fed2229b100c8c7d4f4e69e7a2a2c7383e21
Neel <51184041+neel0210@users.noreply.github.com>
2022-02-04 19:07:28 +0530

    update: Realme debloater

--- Linux/realme.sh
+++ Linux/realme.sh
@@ -28,11 +28,9 @@ adb shell pm uninstall -k --user 0 com.facebook.appmanager
 adb shell pm uninstall -k --user 0 com.vanced.android.youtube
 adb shell pm uninstall -k --user 0 com.facebook.system
 adb shell pm uninstall -k --user 0 com.facebook.appmanager
-adb shell pm uninstall -k --user 0 com.facemoji.lite.xiaomi
 adb shell pm uninstall -k --user 0 com.facebook.appmanager
 adb shell pm uninstall -k --user 0 com.facebook.services
 adb shell pm uninstall -k --user 0 com.facebook.system
-adb shell pm uninstall -k --user 0 com.sec.android.app.DataCreate
 sleep 2
 echo "(--------------------------------------)"
 echo                 Google apps
@@ -51,11 +49,8 @@ adb shell pm uninstall -k --user 0 com.google.android.apps.maps
 adb shell pm uninstall -k --user 0 com.google.android.apps.photos
 adb shell pm uninstall -k --user 0 com.google.android.youtube
 adb shell pm uninstall -k --user 0 com.google.ar.lens
-adb shell pm disable-user --user 0 com.google.ar.lens
-adb shell pm disable-user --user 0 com.google.android.youtube
-adb shell pm disable-user --user 0 com.google.android.gm
-adb shell pm disable-user --user 0 com.google.android.googlequicksearchbox
-adb shell pm disable-user --user 0 com.google.android.apps.maps
+adb shell pm uninstall -k --user 0 com.google.android.apps.youtube.music
+adb shell pm uninstall -k --user 0 com.google.android.keep
 sleep 2
 echo "(--------------------------------------)"
 echo                Microsoft apps
@@ -68,21 +63,25 @@ sleep 2
 echo "(----------------------------------------------------------)"
 echo   Realme apps, ignore if u dont have redmi device and wait 
 echo "(----------------------------------------------------------)"
-adb shell pm uninstall -k --user 0 com.coloros.compass2
 adb shell pm uninstall -k --user 0 com.heytap.market
+adb shell pm uninstall -k --user 0 com.opos.cs
+adb shell pm uninstall -k --user 0 com.heytap.browser
+adb shell pm uninstall -k --user 0 com.heytap.music
+adb shell pm uninstall -k --user 0 com.coloros.video
+adb shell pm uninstall -k --user 0 com.oppo.sos
+adb shell pm uninstall -k --user 0 com.heytap.usercenter
+adb shell pm uninstall -k --user 0 com.heytap.cloud
+adb shell pm uninstall -k --user 0 com.heytap.themestore 
+adb shell pm uninstall -k --user 0 com.finshell.fin
+adb shell pm uninstall -k --user 0 com.coloros.securepay
+adb shell pm uninstall -k --user 0 com.coloros.backuprestore
+adb shell pm uninstall -k --user 0 com.coloros.assistantscreen
 adb shell pm uninstall -k --user 0 com.redteamobile.roaming
-adb shell pm uninstall -k --user 0 com.heytap.market
-adb shell pm disable --user 0 com.heytap.market
-adb shell pm uninstall -k --user 0 --user 0 com.heytap.browser
-adb shell pm uninstall -k --user 0 --user 0 com.heytap.market
-adb shell pm uninstall -k --user 0 --user 0 com.redteamobile.roaming
-adb shell pm uninstall -k --user 0 --user 0 com.heytap.usercenter
-adb shell pm uninstall -k --user 0 --user 0 com.heytap.cloud
-adb shell pm uninstall -k --user 0 --user 0 com.oppo.quicksearchbox
-adb shell pm uninstall -k --user 0 --user 0 com.coloros.backuprestore
-adb shell pm uninstall -k --user 0 --user 0 com.coloros.weather2
-adb shell pm uninstall -k --user 0 --user 0 com.coloros.weather.service
-pm disable-user --user 0 com.nearme.gamecenter
+adb shell pm uninstall -k --user 0 com.coloros.compass2
+adb shell pm uninstall -k --user 0 com.oppo.quicksearchbox
+adb shell pm uninstall -k --user 0 com.realme.link
+adb shell pm uninstall -k --user 0 com.coloros.onekeylockscreen
+adb shell pm uninstall -k --user 0 com.coloros.videoeditor
 sleep 2
 echo "(--------------------------------------------------------------)"
 echo  Exiting script, Now reboot/restart and enjoy debloated Device
